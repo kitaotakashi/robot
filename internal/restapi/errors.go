25367985597b9250b73e01faf4f34068d1839550
kitaotakashi <kitao.takashi@ist.osaka-u.ac.jp>
2021-07-25 16:30:03 +0000

    fix error type

--- internal/restapi/errors.go
+++ internal/restapi/errors.go
@@ -3,7 +3,15 @@ package db
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
+
+type ErrorTips struct{
+	ErrorState 	errorStatesElm	`json:"error_state"`
+	Errors		errorsElm		`json:"errors"`
+	CorporationName	string		`json:"corporation_name"`
+	DepartmentName	string		`json:"department_name"`
+}
 
 func ErrorsView(w http.ResponseWriter, r *http.Request) {
 	db := open()
@@ -29,20 +37,50 @@ func ErrorsView(w http.ResponseWriter, r *http.Request) {
 func ErrorStatesView(w http.ResponseWriter, r *http.Request) {
 	db := open()
 	defer db.Close()
+
+	var error_tips []ErrorTips
+
 	results, err := db.Query("SELECT * FROM error_states")
 	if err != nil {
 		panic(err.Error())
 	}
-	var error_states []errorStatesElm
 	for results.Next() {
+		var error_tip ErrorTips
 		var error_state errorStatesElm
 		columns := columns(&error_state)
 		err = results.Scan(columns...)
 		if err != nil {
 			panic(err.Error())
 		}
-		error_states = append(error_states, error_state)
+		error_tip.ErrorState = error_state
+		
+		var id = error_state.ErrorCode
+		fmt.Println("?")
+		fmt.Println(strconv.Itoa(id))
+
+		results2, err := db.Query("SELECT error_code,error_category,error_message,required_action FROM errors WHERE error_code=" + strconv.Itoa(id))
+		if err != nil {
+			panic(err.Error())
+		}
+		for results2.Next() {
+			var error errorsElm
+			fmt.Println(results2)
+			//columns := columns(&error)
+			//err = results2.Scan(&error)
+			err = results2.Scan(&error.ErrorCode,&error.ErrorCategory,&error.ErrorMessage,&error.RequiredAction)
+			if err != nil {
+				panic(err.Error())
+			}
+			error_tip.Errors.ErrorCode = error.ErrorCode
+			error_tip.Errors.ErrorCategory = error.ErrorCategory
+			error_tip.Errors.ErrorMessage = error.ErrorMessage
+			error_tip.Errors.RequiredAction = error.RequiredAction
+		}
+		error_tip.CorporationName=""
+		error_tip.DepartmentName=""
+
+		error_tips = append(error_tips, error_tip)
 	}
-	fmt.Println(error_states)
-	send(error_states, w)
+	fmt.Println(error_tips)
+	send(error_tips, w)
 }
