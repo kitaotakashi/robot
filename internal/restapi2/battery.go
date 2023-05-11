2047fbd995f8be763edf1c14bd3fa6737e444c38
kitaotakashi <kitao.takashi@ist.osaka-u.ac.jp>
2023-05-11 13:22:32 +0000

    fix batteries api

--- internal/restapi2/battery.go
+++ internal/restapi2/battery.go
@@ -1,166 +1,87 @@
 package mico2
 
 import (
-	"fmt"
+	//"fmt"
 	"net/http"
 	"strconv"
-	"time"
+	//"time"
+	"github.com/joho/godotenv"
+	"fmt"
+	"os"
+	"log"
 )
 
 // UnitsView はunitページに必要なデータをDBから取得し、JSONで返す
 func BatteriesView(w http.ResponseWriter, r *http.Request) {
+	//クエリ取得
 	//id := query(r, "unit_id")
+	q_page := query(r, "page")
+	var _q_page int = 1
+	if len(q_page)>0{
+		_q_page,_ = strconv.Atoi(q_page[0])
+	}
+
+	//env読み込み
+	err := godotenv.Load(fmt.Sprintf("../../%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		log.Fatal(err)
+    }
+	max_data_num,_ := strconv.Atoi(os.Getenv("MAX_DATA_NUM"))
+
 	db := open()
 	defer db.Close()
-	results1, err := db.Query("SELECT * FROM units")
+
+	var res_data []batteryPnt
+	var batteryParent batteryPnt
+	var page pageElm
+	//ページ数を取得
+	results1, err := db.Query("SELECT count(unit_id) FROM units")
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println("chk")
-	fmt.Println(results1)
-
-	var units []unitPnt
-	//unitsベージで必要な情報
-
+	var battery_num int
 	for results1.Next() {
-		var unit unitPnt
-		var unitElm unitElm
-		Columns := columns(&unitElm)
+		err = results1.Scan(&battery_num)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	max_page := int(battery_num/max_data_num)+1
+	if _q_page > max_page{
+		_q_page = max_page
+	}
+	if _q_page <= 0{
+		_q_page = 1
+	}
+	//sql用にデータの開始と終了を取得
+	offset := (_q_page-1)*max_data_num
+	//fmt.Println(_q_page,max_page)
+
+	//test
+	page.PageNow = _q_page
+	page.PageMax = max_page
+
+	var batteries []batteryData
+	results1, err = db.Query("SELECT * FROM units ORDER BY unit_id LIMIT "+strconv.Itoa(max_data_num)+" OFFSET "+strconv.Itoa(offset))
+	if err != nil {
+		panic(err.Error())
+	}
+	for results1.Next() {
+		var battery batteryData
+		var unit	unitData
+		Columns := columns(&unit)
 		err = results1.Scan(Columns...)
 		if err != nil {
 			panic(err.Error())
 		}
-		unit.UnitID = unitElm.UnitID
-		unit.LastIOtime = unitElm.LastIOtime
-
-		if unitElm.BatteryError.Valid == true {
-			errorcode := int(unitElm.BatteryError.Int32)
-			unit.BatteryError = errorcode
-			//error DBから取得
-			results2, err := db.Query("SELECT required_action FROM errors WHERE error_code=" + strconv.Itoa(errorcode))
-			if err != nil {
-				panic(err.Error())
-			}
-			for results2.Next() {
-				var errorElm errorsElm
-				err = results2.Scan(&errorElm.RequiredAction)
-				if err != nil {
-					panic(err.Error())
-				}
-				unit.RequiredAction = errorElm.RequiredAction
-			}
-		}else{
-			unit.RequiredAction = ""
-		}
-
-		//まず battery一覧を取得し、unitIDで問い合わせてcontractIDがあるかどうか
-		//契約IDがnullでない場合、事業所名と企業名を取得
-		var flg=0
-		results3, err := db.Query("SELECT * FROM batteries WHERE unit_id=" + unit.UnitID)
-		
-		for results3.Next() {
-			flg=1
-			var batteryElm batteryElm
-			Columns := columns(& batteryElm)
-			err = results3.Scan(Columns...)
-
-			if err != nil {
-				panic(err.Error())
-			}
-
-			/*
-			var battery_type_id = batteryElm.BatteryTypeID
-			results40, err := db.Query("SELECT type FROM battery_types WHERE battery_type_id=" + strconv.Itoa(battery_type_id))
-			for results40.Next() {
-				var battery_type string
-				
-				err = results40.Scan(&battery_type)
-				if err != nil {
-					panic(err.Error())
-				}
-
-				//detail.Profile.UnitType = batteryElm.BatteryTypeID
-				unit.Profile.UnitType = battery_type
-			}
-
-			unit.ContractID = batteryElm.ContractID
-			unit.Profile.Purpose = batteryElm.Purpose//unitElm.Purpose //from battery DB
-			//unit.Profile.UnitType = "test_RB-***"//<-battery_type_idから製品table取得
-
-			//contractIDから取得
-			//var accountId string
-			results4, err := db.Query("SELECT department_id FROM contracts WHERE contract_id=" + strconv.Itoa(batteryElm.ContractID))
-			for results4.Next() {
-				var contractElm contractElm
-				err = results4.Scan(&contractElm.DepartmentID)
-				if err != nil {
-					panic(err.Error())
-				}
-
-				//acountId = contractElm.AccountID
-				//department_idからparent_id=account_idを取得
-				var departmentElm departmentElm
-				results7, err := db.Query("SELECT parent_id FROM departments WHERE department_id=" + strconv.Itoa(contractElm.DepartmentID))
-				for results7.Next(){
-					
-					err = results7.Scan(&departmentElm.ParentID)
-					if err != nil {
-						panic(err.Error())
-					}
-				}
-
-				//results5, err := db.Query("SELECT corporation_name FROM customers WHERE account_id=" + strconv.Itoa(contractElm.DepartmentID))
-				results5, err := db.Query("SELECT corporation_name FROM customers WHERE account_id=" + strconv.Itoa(departmentElm.ParentID))
-				for results5.Next() {
-					var customerElm customerElm
-					err = results5.Scan(&customerElm.CorporationName)
-					if err != nil {
-						panic(err.Error())
-					}
-
-					unit.CustomerName = customerElm.CorporationName.String	
-				}
-				results6, err := db.Query("SELECT department_name FROM departments WHERE parent_id=" + strconv.Itoa(contractElm.DepartmentID))
-				for results6.Next() {
-					//var departmentElm departmentElm
-					err = results6.Scan(&departmentElm.DepartmentName)
-					if err != nil {
-						panic(err.Error())
-					}
-					unit.DepartmentName = departmentElm.DepartmentName.String
-				}
-				
-			}
-			*/
-			
-		}
-		//契約がない＝customerがない場合
-		if flg==0{
-			unit.ContractID=-1
-			unit.CustomerName = ""	
-			unit.DepartmentName = ""
-		}
-		if unitElm.IsCharging == "close" {
-			unit.IsCharging = false
-		} else {
-			unit.IsCharging = true
-		}
-		if time.Now().Sub(unitElm.LastIOtime) > time.Minute*5 {
-			unit.IsWorking = false
-			//unit.UnitDetail.Status.IsWorking = "off"
-		} else {
-			unit.IsWorking = true
-			//unit.UnitDetail.Status.IsWorking = "on"
-		}
-		unit.Soc = unitElm.Soc
-
-		//unit.UnitDetail.Profile.UnitType = unitElm.UnitType
-		unit.Profile.Location.Latitude = unitElm.Latitude
-		//unit.UnitDetail.Profile.Location.Latitude = unitElm.Latitude
-		unit.Profile.Location.Longitude = unitElm.Longitude
-
-		fmt.Println(unit)
-		units = append(units, unit)
+		battery.Data = unit
+		batteries = append(batteries,battery)
 	}
-	send(units, w)
+	page.DataNum = len(batteries)
+
+	batteryParent.Page = page
+	batteryParent.Data=batteries
+	res_data = append(res_data,batteryParent)
+
+	send(res_data, w)
 }
--- internal/restapi2/type.go
+++ internal/restapi2/type.go
@@ -14,66 +14,35 @@ type Token struct {
 	TokenType	string 	`json:"token_type"`
 }
 
-type batteryElm  struct{
-	SerialNumber	int		`json:"serial_number"`
-	UnitID     		string      `json:"unit_id"`
-	ContractID		int		`json:"contract_id"`
-	BatteryOptionID	int		`json:"battery_option_id"`
-	DateOfManufacture time.Time `json:"date_of_manufacture"`
-	BatteryTypeID	int 	`json:"battery_type_id"`
-	Purpose			string      `json:"purpose"`
-	UnitState		string		`json:"unit_state"`
-}
-
-type geoLocation struct {
-	Latitude  float32 `json:"latitude"`
-	Longitude float32 `json:"longitude"`
-}
-type unitProfile struct {
-	//null.stringに変更すること
-	UnitType string `json:"unit_type"`//battery_type_id
-	Purpose  string `json:"purpose"`
-	Location geoLocation `json:"location"`
-}
-type unitStatus struct {
-	IsCharging bool          `json:"is_charging"`
-	IsWorking  bool          `json:"is_working"`
-	Soc        float32           `json:"soc"`
-	//Soh        sql.NullInt32 `json:"soh"`
-	//Capacity   sql.NullInt32 `json:"capacity"`
-	Current    float32       `json:"current"`
-	Voltage    float32       `json:"voltage"`
-	OutputCurrent    float32     `json:"output_current"`
-	OutputVoltage    float32     `json:"output_voltage"`
-	UsageTime	float32	   `json:"usage_time"`
-	NumberOfCharges int	   `json:"number_of_charges"`
-	MaxCellVoltage float32 `json:"max_cell_voltage"`
-	MinCellVoltage float32 `json:"min_cell_voltage"`
-	MaxTemperature float32 `json:"max_temperature"`
-	MinTemperature float32 `json:"min_temperature"`
-	LastIOtime 	   time.Time   `json:"last_io_time"`
-}
 type unitTimeStamp struct {
 	RegisterdAt time.Time `json:"registerd_at"`
 	Time        time.Time `json:"time"`
 }
 
-type unitPnt struct{
-	UnitID         string      `json:"unit_id"`//units_DBから取得
-	CustomerName   string      `json:"customer_name"`//contract_DBから取得
-	DepartmentName string      `json:"department_name"`//contract_DBから取得
-	ContractID int `json:"contract_id"`//contract_DBから取得
-	LastIOtime 	   time.Time   `json:"last_io_time"`
-	//ContractName   string      `json:"contract_name"`
-	Profile        unitProfile `json:"profile"`
-	IsCharging     bool        `json:"is_charging"`//units_DBから取得
-	IsWorking      bool        `json:"is_working"`//last_io_timeから別途計算
-	Soc            float32     `json:"soc"`//units_DBから取得
-	RequiredAction string      `json:"required_action"`//error_DBから取得
-	BatteryError  	int   `json:"battery_error""`//units_DBから取得
+type pageElm struct{
+	PageNow	int		`json:"page_now"`
+	PageMax	int		`json:"page_max"` 
+	DataNum int		`json:"data_num"`
 }
 
-type unitElm struct { //要素名を変更
+type batteryPnt struct{
+	Page	pageElm			`json:"page"`
+	Data	[]batteryData	`json:"batteries"`
+}
+
+type batteryData struct{
+	Data 		unitData 		`json:"data"`
+	Management 	managementElm	`json:"management"`
+}
+
+type managementElm struct{
+	IsRestration 	bool		`json:"is_registration"`
+	SerialNumber 	string		`json:"serial_number"`
+	Customer 		string		`json:"customer"`
+	IsError			bool		`json:"is_error"`	
+}
+
+type unitData struct{
 	UnitID     string      `json:"unit_id"`
 	Time       time.Time   `json:"time"`
 	BmsVersion string `json:"bms_version"`
@@ -97,17 +66,6 @@ type unitElm struct { //要素名を変更
 	MinCellVoltage float32 `json:"min_cell_voltage"`
 	MaxTemperature float32 `json:"max_temperature"`
 	MinTemperature float32 `json:"min_temperature"`
-
-	/*
-	UnitType   null.String `json:"unit_type"`
-	Purpose    null.String `json:"purpose"`
-	UnitState  null.String `json:"unit_state"`
-	IsWorking  string    `json:"is_working"`
-	Soh        sql.NullInt32 `json:"soh"`
-	Capacity   sql.NullInt32 `json:"capacity"`
-	ErrorCode  sql.NullInt32 `json:"error_code"`
-	//ContractID sql.NullInt32 `json:"contract_id"` -> db側で実装まち
-	*/
 }
 
 type errorsElm struct {
