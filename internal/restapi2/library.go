a766e382d6cd6f3af6566986db29cfef1d14fd70
kitaotakashi <kitao.takashi@ist.osaka-u.ac.jp>
2025-03-17 01:02:33 +0000

    fix displayed data of bmu

--- internal/restapi2/library.go
+++ internal/restapi2/library.go
@@ -12,6 +12,7 @@ import (
 	"time"
 	"regexp"
 	"strconv"
+	"math"
 )
 
 // query はkeyがkのクエリパラメータを返す
@@ -146,12 +147,49 @@ func CheckInt(_x string)bool{
 }
 
 func TransferBMUtoUnitData(bmuData unitBMUData) unitData {
+
+	charge_mode := "充電OFF,出力OFF";
+	if bmuData.FetState == "01"{
+		charge_mode = "充電OFF,出力ON"
+	}else if bmuData.FetState == "02"{
+		charge_mode = "充電ON, 出力OFF"
+	}else if bmuData.FetState == "03"{
+		charge_mode = "充電ON, 出力ON"
+	}
+
+	battery_current := float32(0.0)
+	ADZERO := float32(3300.0)
+	GIM := float32(60.0)
+	RS := float32(0.5)
+
+	diff := ADZERO - bmuData.ViSense50
+	if diff > 0{
+		battery_current = (float32)(diff * (2.5 / 65536) / GIM / RS)
+	}
+
+	battery_voltage := bmuData.VCell6 + bmuData.VCell7 + bmuData.VCell8 + bmuData.VCell9 + bmuData.VCell10 + bmuData.VCell11 + bmuData.VCell12 + bmuData.VCell13
+
     // Energy100が0の場合はゼロ除算を防ぐ
     socValue := float32(0.0)
+	//if bmuData.Energy == "00"
     if bmuData.Energy100 != 0 {
         socValue =100 * float32(bmuData.Energy) / float32(bmuData.Energy100)
     }
 
+	vmin, vmax := GetMinMax(bmuData)
+
+	Rthc := float32(0.0)
+	if bmuData.TAdr0 < 4096{
+		Rthc = float32(10000 * bmuData.TAdr0/(4096 - bmuData.TAdr0))
+	}
+	max_temperature := 3435.0 / (math.Log(float64(Rthc)/10000.0) + (3435.0 / 25.0))
+
+	Rthc2 := float32(0.0)
+	if bmuData.TAdr1 < 4096{
+		Rthc2 = float32(10000 * bmuData.TAdr1/(4096 - bmuData.TAdr1))
+	}
+	min_temperature := 3435.0 / (math.Log(float64(Rthc2)/10000.0) + (3435.0 / 25.0))
+	
     return unitData{
         UnitID:         fmt.Sprintf("%d", bmuData.BmuID), // BmuID を UnitID に変換（string型）
         Time:           bmuData.Time,                   // Time
@@ -159,20 +197,44 @@ func TransferBMUtoUnitData(bmuData unitBMUData) unitData {
         LastIOtime:     bmuData.LastIOTime,             // LastIOTime
         Longitude:      0.0,                            // Longitude (初期値)
         Latitude:       0.0,                            // Latitude (初期値)
-        ChargeMode:     "",                             // ChargeMode (初期値)
-        BatteryCurrent: 0.0,                            // BatteryCurrent (初期値)
-        BatteryVoltage: 0.0,                            // BatteryVoltage (初期値)
+        ChargeMode:     charge_mode,                             // ChargeMode (初期値)
+        BatteryCurrent: battery_current,                            // BatteryCurrent (初期値)
+        BatteryVoltage: battery_voltage,                            // BatteryVoltage (初期値)
         BatteryError:   sql.NullInt32{},                // BatteryError (初期値: NULL)
         Soc:            socValue,                       // Soc を Energy / Energy100 で計算
-        OutputCurrent:  0.0,                            // OutputCurrent (初期値)
-        OutputVoltage:  0.0,                            // OutputVoltage (初期値)
+        OutputCurrent:  battery_current,                            // OutputCurrent (初期値)
+        OutputVoltage:  battery_voltage,                            // OutputVoltage (初期値)
         IsCharging:     "",                             // IsCharging (初期値)
         ChargerError:   0,                              // ChargerError (初期値)
         UsageTime:      0.0,                            // UsageTime (初期値)
         NumberOfCharges: int(bmuData.ChargeNum),             // ChargeNum を NumberOfCharges にマッピング
-        MaxCellVoltage: bmuData.VCell0,                 // VCell0 を MaxCellVoltage にマッピング
-        MinCellVoltage: bmuData.VCell13,                // VCell13 を MinCellVoltage にマッピング
-        MaxTemperature: 0.0,                            // MaxTemperature (初期値)
-        MinTemperature: 0.0,                            // MinTemperature (初期値)
+        MaxCellVoltage: vmax,                 // VCell0 を MaxCellVoltage にマッピング
+        MinCellVoltage: vmin,                // VCell13 を MinCellVoltage にマッピング
+        MaxTemperature: float32(max_temperature),                            // MaxTemperature (初期値)
+        MinTemperature: float32(min_temperature),                            // MinTemperature (初期値)
     }
+}
+
+func GetMinMax(bmu unitBMUData) (min, max float32) {
+	values := []float32{
+		bmu.VCell6,
+		bmu.VCell7,
+		bmu.VCell8,
+		bmu.VCell9,
+		bmu.VCell10,
+		bmu.VCell11,
+		bmu.VCell12,
+		bmu.VCell13,
+	}
+
+	min, max = values[0], values[0]
+	for _, v := range values {
+		if v < min {
+			min = v
+		}
+		if v > max {
+			max = v
+		}
+	}
+	return
 }
